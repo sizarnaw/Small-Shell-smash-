57054c1c6d83dc45c34dfeed0fc433a806376fbc
sizarnaw <61415431+sizarnaw@users.noreply.github.com>
2021-11-19 23:16:15 +0200

    Add files via upload

---
+++ Commands.cpp
@@ -0,0 +1,169 @@
+#include <unistd.h>
+#include <string.h>
+#include <iostream>
+#include <vector>
+#include <sstream>
+#include <sys/wait.h>
+#include <iomanip>
+#include "Commands.h"
+
+using namespace std;
+
+#if 0
+#define FUNC_ENTRY()  \
+  cout << __PRETTY_FUNCTION__ << " --> " << endl;
+
+#define FUNC_EXIT()  \
+  cout << __PRETTY_FUNCTION__ << " <-- " << endl;
+#else
+#define FUNC_ENTRY()
+#define FUNC_EXIT()
+#endif
+
+string _ltrim(const std::string& s)
+{
+  size_t start = s.find_first_not_of(WHITESPACE);
+  return (start == std::string::npos) ? "" : s.substr(start);
+}
+
+string _rtrim(const std::string& s)
+{
+  size_t end = s.find_last_not_of(WHITESPACE);
+  return (end == std::string::npos) ? "" : s.substr(0, end + 1);
+}
+
+string _trim(const std::string& s)
+{
+  return _rtrim(_ltrim(s));
+}
+
+int _parseCommandLine(const char* cmd_line, char** args) {
+  FUNC_ENTRY()
+  int i = 0;
+  std::istringstream iss(_trim(string(cmd_line)).c_str());
+  for(std::string s; iss >> s; ) {
+    args[i] = (char*)malloc(s.length()+1);
+    memset(args[i], 0, s.length()+1);
+    strcpy(args[i], s.c_str());
+    args[++i] = NULL;
+  }
+  return i;
+
+  FUNC_EXIT()
+}
+
+bool _isBackgroundComamnd(const char* cmd_line) {
+  const string str(cmd_line);
+  return str[str.find_last_not_of(WHITESPACE)] == '&';
+}
+
+void _removeBackgroundSign(char* cmd_line) {
+  const string str(cmd_line);
+  // find last character other than spaces
+  unsigned int idx = str.find_last_not_of(WHITESPACE);
+  // if all characters are spaces then return
+  if (idx == string::npos) {
+    return;
+  }
+  // if the command line does not end with & then return
+  if (cmd_line[idx] != '&') {
+    return;
+  }
+  // replace the & (background sign) with space and then remove all tailing spaces.
+  cmd_line[idx] = ' ';
+  // truncate the command line string up to the last non-space character
+  cmd_line[str.find_last_not_of(WHITESPACE, idx) + 1] = 0;
+}
+
+// TODO: Add your implementation for classes in Commands.h
+void ChPromptCommand::execute() {
+    SmallShell& smash = SmallShell::getInstance();
+    if(arguments.size() == 1){
+        smash.changePromptName();
+    }else if(arguments.size() == 2){
+        smash.changePromptName(arguments[1]);
+    }else{
+        //TODO Error Handling
+    }
+}
+void ShowPidCommand::execute() {
+    if(arguments.size() > 1)
+        return;
+    cout<<"smash pid is "<<SmallShell::getInstance().GetPID()<<endl;
+}
+void GetCurrDirCommand::execute() {
+    if(arguments.size() > 1)
+        return;
+    cout<<get_current_dir_name()<<endl;
+}
+void ChangeDirCommand::execute() {
+    SmallShell& smash = SmallShell::getInstance();
+    if(arguments.size() == 2){
+        if(arguments[1] == '-'){
+            chdir(smash.)
+        }
+    }
+}
+SmallShell::SmallShell() {
+// TODO: add your implementation
+    promptName = "smash";
+    lastPWD = nullptr;
+    PID = getpid();
+}
+
+SmallShell::~SmallShell() {
+// TODO: add your implementation
+    delete lastPWD;
+}
+
+/**
+* Creates and returns a pointer to Command class which matches the given command line (cmd_line)
+*/
+Command::Command(const char *cmd_line) {
+    string s = string(cmd_line);
+    string temp;
+    for (int i = 0; i < s.size(); ++i) {
+        if (s[i] != ' ') {
+            temp.push_back(s[i]);
+        } else {
+            arguments.push_back(temp);
+            temp.clear();
+            while (i < s.size() && s[i] == ' ') {
+                i++;
+            }
+        }
+    }
+}
+BuiltInCommand::BuiltInCommand(const char *cmd_line) {
+
+}
+
+
+Command * SmallShell::CreateCommand(const char* cmd_line) {
+	// For example:
+    //cmd_s command without spaces
+  string cmd_s = _trim(string(cmd_line));
+  string firstWord = cmd_s.substr(0, cmd_s.find_first_of(" \n"));
+
+
+  if (firstWord.compare("chprompt") == 0) {
+      return new ChPromptCommand();
+  } else if (firstWord.compare("showpid") == 0) {
+      return new ShowPidCommand();
+  } else if (firstWord.compare("pwd") == 0) {
+      return new GetCurrDirCommand();
+  } else if (firstWord.compare("cd") == 0) {
+      return new ChangeDirCommand(); //TODO: saweha
+  } else {
+      return new ExternalCommand(cmd_line);
+  }
+
+  return nullptr;
+}
+void SmallShell::executeCommand(const char *cmd_line) {
+  // TODO: Add your implementation here
+  // for example:
+  // Command* cmd = CreateCommand(cmd_line);
+  // cmd->execute();
+  // Please note that you must fork smash process for some commands (e.g., external commands....)
+}
